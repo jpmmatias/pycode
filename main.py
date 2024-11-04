cc93b1c1f01422ed2412e6934106315af55da052
João Pedro Matias <jpmmatias2018@outlook.com>
2024-11-04 06:57:26 -0300

    feat - adding test chain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from langchain_community.llms import OpenAI
 from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain 
+from langchain.chains import LLMChain, SequentialChain
 from dotenv import load_dotenv
 import argparse
 
@@ -24,14 +24,32 @@ code_prompt = PromptTemplate(
 
 code_chain = LLMChain(
     llm = llm,
-    prompt=code_prompt
+    prompt=code_prompt,
+    output_key="code"
 )
 
-result = code_chain.invoke({
+test_prompt = PromptTemplate(
+    input_variables=[ "code"],
+    template="Write a simple test that will test this code: {code}",
+)
+
+test_chain= LLMChain(
+    llm = llm,
+    prompt=test_prompt,
+    output_key="test"
+)
+
+chain = SequentialChain(
+    chains=[code_chain, test_chain],
+    input_variables=["task", "language"],
+    output_variables=["test", "code"]
+)
+
+result = chain({
     "language": args.language,
     "task":args.task
 })
 
-print(result["text"])
+print(result["test"])
 
 
